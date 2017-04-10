a48c73b4e647789a52a3323b42fba45865cd39f9
mihaisoldan <catalinmihai.soldan@studenti.unicam.it>
2017-04-11 00:11:22 +0200

    Aggiunta classe Cavallo

---
+++ Modello/Pezzi/Cavallo.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Scacchi.Modello.Pezzi{
+    public class Cavallo : IPezzo
+    {
+        private readonly Colore colore;
+
+        public Cavallo(Colore colore){
+            this.colore = colore;
+        }
+
+        public Colore Colore{
+            get{
+                return colore;
+            }
+        }
+
+        public bool PuòMuovere(Colonna colonnaPartenza, Traversa traversaPartenza, Colonna colonnaArrivo, Traversa traversaArrivo)
+        {
+            var distanzaTraLeColonne = Math.Abs(colonnaPartenza - colonnaArrivo);
+            var distanzaTraLeTraverse = Math.Abs(traversaPartenza - traversaArrivo);
+
+            if((distanzaTraLeColonne==2 && distanzaTraLeTraverse==1)||(distanzaTraLeTraverse==2 && distanzaTraLeColonne ==1))
+                return true;
+            return false;
+        }
+    }
+}
