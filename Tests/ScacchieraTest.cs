9837387801a06b71984044d4de5905bd5b3cb76d
mihaisoldan <catalinmihai.soldan@studenti.unicam.it>
2017-04-14 16:48:40 +0200

    Modificato il nome del file dei test per la scacchiera in ScacchieraTest.cs

---
+++ Tests/ScacchieraTest.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using Scacchi.Modello;
+using Xunit;
+
+namespace Scacchi.Tests{
+
+  public class ScacchieraTest{
+
+    [Fact]
+    public void ScacchieraDeveAvere64Case()
+    {
+      //Given
+      IScacchiera scacchiera= new Scacchiera();
+      //When
+
+      //Then
+      Assert.Equal(64, scacchiera.Case.Count());
+    }
+    [Fact]
+    public void indexerDeveRestituireCasaCorretta()
+    {
+      //Given
+      IScacchiera scacchiera= new Scacchiera();
+      //When
+      ICasa casa= scacchiera[Colonna.F,Traversa.Quinta];
+      //Then
+      Assert.Equal(Colonna.F, casa.Colonna);
+      Assert.Equal(Traversa.Quinta, casa.Traversa);
+    }
+
+    [Fact]
+    public void sePezzoNonPresenteIlGetterRitornaNull(){
+      //Given
+      IScacchiera scacchiera = new Scacchiera();
+      //When
+      ICasa casa = scacchiera[Colonna.B,Traversa.Prima];
+      //Then
+      Assert.Null(casa.PezzoPresente);
+    }
+  }
+}
